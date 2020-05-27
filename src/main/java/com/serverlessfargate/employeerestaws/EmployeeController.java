1b954ef7819ea94ac07997af306945124e85adcd
manvendrasingh <manvendra80386@gmail.com>
2020-05-27 22:53:01 +0530

    Added more logic and Modified Dockerfile

--- src/main/java/com/serverlessfargate/employeerestaws/EmployeeController.java
+++ src/main/java/com/serverlessfargate/employeerestaws/EmployeeController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("/aws")
 public class EmployeeController {
 
   @GetMapping("/employees")
